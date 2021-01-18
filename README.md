33bc3bda12c20c7002085e30e6458535b84ffe58
MengJia <jiameng15@126.com>
2021-01-18 14:54:58 +0800

    update

--- README.md
+++ README.md
@@ -2,12 +2,44 @@
 
 > 前端 AES 加解密的实现
 
-## 参考文章
-感兴趣的小伙伴可以参考我的这篇文章[前端 crypto-js aes 加解密](https://juejin.im/post/5acf3b0c5188255c620f781b)
+## 如何开始
+
+### 第一步
+
+引入依赖:
+
+```
+npm install crypto-js
+```
+
+### 第二步
+
+在项目中创建 secret.js
+本示例文件路径为 /src/utils/secret.js
+
+### 第三步
+
+在需要的页面中使用
+引入及使用加密解密方法
+
+```
+import secret from "../utils/secret";
+
+methods: {
+  encrypt() {
+    this.userInfo = this.encryptMsg = secret.Encrypt('需要加密的内容');
+  },
+  decrypt() {
+    this.decryptMsg = secret.Decrypt('需要解密的内容');
+  },
+}
+```
+
+具体参考 /src/view/index.vue
 
 ## Build Setup
 
-``` bash
+```bash
 # install dependencies
 npm install
 
@@ -20,5 +52,3 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/utils/secret.js
+++ src/utils/secret.js
@@ -1,24 +1,27 @@
-const CryptoJS = require('crypto-js');  //引用AES源码js
+const CryptoJS = require("crypto-js"); //引用AES源码js
 
-const key = CryptoJS.enc.Utf8.parse("1234123412ABCDEF"); //十六位十六进制数作为秘钥
-const iv = CryptoJS.enc.Utf8.parse('ABCDEF1234123412'); //十六位十六进制数作为秘钥偏移量
+const key = CryptoJS.enc.Utf8.parse("abcdefgabcdefg12"); //十六位十六进制数作为秘钥
 
 //解密方法
 function Decrypt(word) {
-    let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
-    let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
-    let decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
-    let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
-    return decryptedStr.toString();
+  let decrypt = CryptoJS.AES.decrypt(word, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+  let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+  return decryptedStr.toString();
 }
 //加密方法
 function Encrypt(word) {
-    let srcs = CryptoJS.enc.Utf8.parse(word);
-    let encrypted = CryptoJS.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
-    return encrypted.ciphertext.toString().toUpperCase();
+  let srcs = CryptoJS.enc.Utf8.parse(word);
+  let encrypted = CryptoJS.AES.encrypt(srcs, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+  return encrypted.toString();
 }
 
 export default {
-	Decrypt ,
-	Encrypt
-}
+  Decrypt,
+  Encrypt,
+};
